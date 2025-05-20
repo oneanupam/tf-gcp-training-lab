58d7ad468cabe29e121db2a9a59f9d989cfe3375
anupam. <43881706+oneanupam@users.noreply.github.com>
2025-05-21 00:18:45 +0530

    chore: kzb-20 bump tf version to 1.12.0 (#33)

--- tf-cli_workspaces/versions.tf
+++ tf-cli_workspaces/versions.tf
@@ -1,11 +1,11 @@
 // Terraform block to configure terraform and provider version
 terraform {
-  required_version = "~> 1.3.6"
+  required_version = "~> 1.12.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.55.0"
+      version = "~> 6.35.0"
     }
   }
 }
