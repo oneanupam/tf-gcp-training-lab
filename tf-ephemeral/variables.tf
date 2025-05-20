cf6d3b56a4b86b7b405e84907a2f8c20c2b5110d
Anupam <anupaminit@gmail.com>
2025-05-20 13:20:03 +0530

    chore: kzb-20 add ephemeral variable

--- tf-ephemeral/variables.tf
+++ tf-ephemeral/variables.tf
@@ -17,6 +17,13 @@ variable "terraform_service_account" {
   # Make sure to give "roles/iam.serviceAccountTokenCreator" role to an identity (who will trigger the terraform code) on this service account for the impersonation to succeed.
 }
 
+variable "override_special_characters" {
+  type        = string
+  description = "value to override special characters in the password generation."
+  default     = "#$&*"
+  ephemeral   = true
+}
+
 variable "revision" {
   description = "The revision number of the secret to be used."
   type        = number
