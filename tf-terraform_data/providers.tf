f301e4f15d849a4d50e0a8d000b0318a1ca83597
Anupam <anupaminit@gmail.com>
2025-07-11 12:24:20 +0530

    feat: kzb-20 add terraform data examples

--- tf-terraform_data/providers.tf
+++ tf-terraform_data/providers.tf
@@ -1,4 +1,11 @@
+// Provider configuration for Google Cloud Platform
 provider "google" {
+  project = var.project_id
+  region  = var.default_region
+  zone    = var.default_zone
+}
+
+provider "google-beta" {
   project = var.project_id
   region  = var.default_region
   zone    = var.default_zone
