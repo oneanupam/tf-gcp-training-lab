e52f0dc332b44c13c4c29795fdf57a7304efc93a
Anupam <anupaminit@gmail.com>
2025-07-13 00:53:02 +0530

    feat: kzb-20 add custom conditions code

--- tf-custom_conditions/checks.tf
+++ tf-custom_conditions/checks.tf
@@ -1,7 +1,12 @@
 # Check block to validate the deployed infrastructure
 check "tst_vpc_exists" {
+
+  data "google_compute_network" "tst_vpc" {
+    name = var.vpc_name
+  }
+
   assert {
-    condition     = google_compute_network.tst_vpc.self_link != ""
+    condition     = data.google_compute_network.tst_vpc.self_link != ""
     error_message = "VPC was not created or self_link is missing"
   }
 }
