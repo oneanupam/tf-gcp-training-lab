4449086b37c2cb0ade419bf2b7d33b75bfcd4b4d
Anupam <anupaminit@gmail.com>
2025-07-14 14:45:30 +0530

    feat: bump tf version to 1.12.2

---
+++ terraform/tf-import_command/versions.tf
@@ -0,0 +1,15 @@
+// Terraform block to configure terraform and provider version
+terraform {
+  required_version = "~> 1.12.2"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.43.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 6.43.0"
+    }
+  }
+}
